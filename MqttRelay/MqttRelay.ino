86607fdbf8b5238440c078b7c12ce0cf70985de2
marcoprovolo <34738514+marcoprovolo@users.noreply.github.com>
2018-02-09 18:20:41 +0100

    Add files via upload

--- MqttRelay/MqttRelay.ino
+++ MqttRelay/MqttRelay.ino
@@ -5,23 +5,9 @@
         - reconnect() connect to the broker and SUBSRIBE to topics
         - callback() recieve the messages this client is subscribed to
     - turnON/turnOFF function
-    
+
     payload on = "ON"
-    payload off = "OFF"
-    
-    home assistant config:
-    switch:
-      platform: mqtt
-      name: "my first controlled relay"
-      command_topic: "myESP/setPower" 
-#or if headerTopic is changed ->  "{headertopic}/setPower"
-      state_topic: "myESP/powerSet"
-#or if headerTopic is changed ->  "{headertopic}/powerSet"  
-      payload_on: "ON"
-      payload_off: "OFF"
-      optimistic: false
-      qos: 1
-      retain: true
+    payload off = "OFF"   
 */
 
 #include <ESP8266WiFi.h>      //https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi
@@ -31,7 +17,6 @@
 /************ WIFI and MQTT AUTENTICATION ******************/
 /****************    CUSTOMIZE THIS    *********************/
 /*                       ||||||
-                         ||||||
                          ||||||
                         \||||||/
                          \||||/
@@ -44,29 +29,29 @@
 #define mqtt_port 1883                    //your MQTT broker port
 #define mqtt_user "super_cool_username"   //your MQTT username (if not set the parameter is ignored by the broker)
 #define mqtt_password "SuperSecretPSWD"   //your MQTT password (if not set the parameter is ignored by the broker)
-char headerTopic[64] = "myESP";           //this will create a myESP/{topic identifier}
+
+#define MAX_TOPIC_LENGTH 64
+char topicHeader[MAX_TOPIC_LENGTH] = "myESPrelay";           //this will create a myESPrelay/{topic identifier}
+
 
 #define relayPin D1  //define pin
 
 /****************************** MQTT TOPICS  ***************************************/
-char RelayPowerSub[64] = "setPower";    //  turn ON turn OFF
-char RelayPowerPub[64]  = "powerSet";    //  and confirm
+char relayPowerSub[MAX_TOPIC_LENGTH] = "setPower";    //  turn ON turn OFF
+char relayPowerPub[MAX_TOPIC_LENGTH]  = "powerSet";    //  and confirm
 
 WiFiClient espClient;                     //initialise a wifi client
-PubSubClient client(espClient);         //creates a partially initialised client instance for MQTT connection
+PubSubClient client(espClient);           //creates a partially initialised client instance for MQTT connection
 char msg_buff[50];                        //for storing incoming/outcoming messages
 String topic;
 String payload;
-int RelayState = 0;                       //initial state of relay
 
 void setup_wifi() {
   randomSeed(micros());
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(wifi_ssid);
-
   WiFi.begin(wifi_ssid, wifi_password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -89,7 +74,7 @@ void reconnect() {
     if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
       Serial.println("connected");
       // Once (re)connected (re)subscribe
-      client.subscribe(RelayPowerSub);
+      client.subscribe(relayPowerSub);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -119,11 +104,11 @@ void callback(char* c_topic, byte* c_payload, unsigned int c_length) {
 
   if (payload == "ON") {
     digitalWrite(relayPin, HIGH);   // Turn the LED on
-    client.publish(RelayPowerPub, msg_buff);
+    client.publish(relayPowerPub, msg_buff);
   }
   if (payload == "OFF") {
     digitalWrite(relayPin, LOW);   // Turn the LED on
-    client.publish(RelayPowerPub, msg_buff);
+    client.publish(relayPowerPub, msg_buff);
   }
 }
 void setup() {
@@ -132,27 +117,38 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
   digitalWrite(relayPin, LOW);
-  StringComposer(headerTopic, RelayPowerPub);
+  topicComposer(relayPowerPub);
   Serial.print("Your relay Command topic: ");
-  Serial.println(RelayPowerPub);
-  StringComposer(headerTopic, RelayPowerSub);
+  Serial.println(relayPowerPub);
+  topicComposer(relayPowerSub);
   Serial.print("Your relay Status topic: ");
-  Serial.println(RelayPowerSub);
+  Serial.println(relayPowerSub);
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);  //client is now ready for use
   client.setCallback(callback);              //client is now listening for incoming messages
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 }
-//                       HEADER         TOPIC
-void StringComposer(char header[], char actionTopic[]){  
-  String stringComposer = String(header) + "/"+ String(actionTopic);
-  stringComposer.toCharArray(actionTopic, 64);
+
+void topicComposer(char actionTopic[]) {
+  //                                    TOPICHEADER/ACTION_TOPIC
+  String stringComposer = String(topicHeader) + "/" + String(actionTopic);
+   if ( stringComposer.length() >= MAX_TOPIC_LENGTH) {
+    Serial.print(" ERROR!! \nlength of topic exceed the max value of ");
+    Serial.print(MAX_TOPIC_LENGTH - 1);
+    Serial.print(" characters. Impossible to create such a topic: ");
+    Serial.println(stringComposer);
+    Serial.print("your topic is: ");
+    Serial.println(actionTopic);
+  } else {
+    stringComposer.toCharArray(actionTopic, stringComposer.length() + 1);
+    Serial.println(actionTopic);
+  }
 }
 
+/****** END OF FILE ******/
